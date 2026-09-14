6a4107ab3d0387b5885f37be5c87c1ebf7b9a1c9
Gustaf Järgren <gurreja@gmail.com>
2026-09-14 17:26:42 +0200

    Simplified the template replacing

--- fa2cs/CodeWriter.cs
+++ fa2cs/CodeWriter.cs
@@ -15,25 +15,20 @@ public static class CodeWriter
         var classTemplate = ResourcesHelper.ReadResourceContent("ClassTemplate.txt");
         var propertyTemplate = ResourcesHelper.ReadResourceContent("PropertyTemplate.txt");
 
-        var properties = icons
-            .Select(icon => new
-            {
-                icon,
-                property = propertyTemplate.Replace("$link$", icon.Url)
-                    .Replace("$name$", icon.Id)
-                    .Replace("$code$", icon.Unicode)
-                    .Replace("$dotnet_name$", icon.DotNetName)
-                    .Replace("$introduced_version$", icon.IntroducedVersion)
-                    .Replace("$last_modified_version$", icon.LastModifiedVersion)
-                    .Replace("$styles$", icon.StylesSummary)
-                    .Replace("$supported_pro_fonts$", string.Join(", ", icon.ProStyles.Select(s => $"FAStyle.{s}")))
-            })
-            .Select(t =>
-                t.property.Replace("$supported_free_fonts$",
-                    t.icon.FreeStyles.Any()
-                        ? string.Join(", ", t.icon.FreeStyles.Select(s => $"FAStyle.{s}"))
-                        : "FAStyle.Unsupported")
-            ).ToList();
+        var properties = icons.Select(icon => propertyTemplate
+                .Replace("$link$", icon.Url)
+                .Replace("$name$", icon.Id)
+                .Replace("$code$", icon.Unicode)
+                .Replace("$dotnet_name$", icon.DotNetName)
+                .Replace("$introduced_version$", icon.IntroducedVersion)
+                .Replace("$last_modified_version$", icon.LastModifiedVersion)
+                .Replace("$styles$", icon.StylesSummary)
+                .Replace("$supported_pro_fonts$", string.Join(", ", icon.ProStyles.Select(s => $"FAStyle.{s}")))
+                .Replace("$supported_free_fonts$",
+                    icon.FreeStyles.Any()
+                        ? string.Join(", ", icon.FreeStyles.Select(s => $"FAStyle.{s}"))
+                        : "FAStyle.Unsupported"))
+            .ToList();
 
         var separator = Environment.NewLine + Environment.NewLine;
         var code = string.Join(separator, properties);
